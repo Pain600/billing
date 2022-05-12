29f4a63db86c0d6f655b199263b3138dd9d71d40
potatogallery <khazsaunders@hotmail.com>
2022-05-12 22:55:30 +0200

    FINAL UPDATE

--- payment-page-master/main.js
+++ payment-page-master/main.js
@@ -1,11 +1,15 @@
+function redirpayment() {
+  alert('Your Total Bill : R 84.99');
+  location.replace("payment.html")
+}
+
+
 let btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
-    let billAmount = document.getElementById('bill-amount').value;
-    let tipPercentage = document.getElementById('vat-percentage').value;
+    let billAmount = document.getElementById('bill-amount').value=69.99;
+    let tipPercentage = document.getElementById('vat-percentage').value=15;
 
-    
-    /*let tipAmount = document.getElementById('total-bill').value = billAmount / tipPercentage;*/
     document.getElementById('total-bill').value = parseFloat(billAmount) + parseFloat(tipPercentage);
 
 })
